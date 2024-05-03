f087cf90ee166f8b618569638dc79fb3795cb2e7
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2024-05-03 21:24:04 +0100

    fix missing Werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-package { 'flask':
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
-
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => pip3,
+}
