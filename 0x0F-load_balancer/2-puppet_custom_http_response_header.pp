a257394ae00840407307cac1d34500caf73c41ed
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-05-18 04:51:58 -0700

    fix 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,17 +6,14 @@ exec { 'update':
   command => '/usr/bin/apt-get update',
 }
 
-package { 'nginx':
-  ensure => 'installed',
+-> package {'nginx':
+  ensure => 'present',
 }
-
 -> file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
   match => 'http {',
   line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
 -> exec {'run':
   command => '/usr/sbin/service nginx restart',
 }
-
