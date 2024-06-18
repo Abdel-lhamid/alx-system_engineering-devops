714292ac9a122056cc4974e98a74b1afb5d48e28
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-06-18 16:01:19 +0100

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns
+the number of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """return nb of subscribers for a subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'alx-stud-linux'})
+    req = requests.get(url, headers=headers).json()
+    subs = r.get('data', {}).get('subscribers')
+    if response.status_code == 404 or not subs:
+        return 0
+    return subs
