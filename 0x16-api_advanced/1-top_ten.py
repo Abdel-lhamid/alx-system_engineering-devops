7836a0e951b32ec380cbf35cea73a14f71171b1d
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-06-19 13:17:02 +0100

    limit to 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,13 +9,13 @@ def top_ten(subreddit):
     """
     queries the first 10 hot posts listed for a given subreddit
     """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=9".format(subreddit)
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'alx-stud-linux'})
     req = requests.get(url, headers=headers, allow_redirects=False)
     if req.status_code == 404:
         print(None)
         return
-    posts = req.json().get("data").get("children")
+    posts = req.json().get('data').get('children')
     for post in posts:
-        print(post.get("data").get("title"))
+        print(post.get('data').get('title'))
