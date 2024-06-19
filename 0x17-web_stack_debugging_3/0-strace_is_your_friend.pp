75943fb9d3532fafcea6fa5fb03e7c1b52145e66
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-06-19 13:57:38 +0100

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# puppet fix for
+
+exec { 'fix-wordpress':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,5 @@
+# 0x17. Web stack debugging #3
+
+| File      | Description |
+| ----------- | ----------- |
+| [0-strace_is_your_friend.pp](./0-strace_is_your_friend.pp) | 0-strace_is_your_friend.pp |
